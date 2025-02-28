8214ce576b7f08a234d6316374b2a237070f0bb4
Avram Walden <aviemet@gmail.com>
2025-02-28 07:55:15 -0800

    fix: updates sass for modern deprecations

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,6 +12,49 @@ const config: StorybookConfig = {
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
     "@storybook/blocks",
+    // "@storybook/addon-styling-webpack",
+    {
+      name: '@storybook/addon-styling-webpack',
+      options: {
+        rules: [
+          // Replaces any existing Sass rules with given rules
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              "style-loader",
+              "css-loader",
+              {
+                loader: "sass-loader",
+                options: { implementation: require.resolve("sass") },
+              },
+            ],
+          },
+        ],
+      },
+    },
+    // {
+    //   name: '@storybook/addon-styling-webpack',
+    //   options: {
+    //     rules: [
+    //       // Replaces existing CSS rules to support PostCSS
+    //       {
+    //         test: /\.css$/,
+    //         use: [
+    //           'style-loader',
+    //           {
+    //             loader: 'css-loader',
+    //             options: { importLoaders: 1 },
+    //           },
+    //           {
+    //             // Gets options from `postcss.config.js` in your project root
+    //             loader: 'postcss-loader',
+    //             options: { implementation: require.resolve('postcss') },
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    // },
   ],
   framework: {
     name: "@storybook/react-webpack5",
@@ -22,12 +65,38 @@ const config: StorybookConfig = {
     defaultName: 'Documentation',
   },
   webpackFinal: async (config) => {
+    if(!config.module) config.module = { rules: [] }
+    if(!config.resolve) config.resolve = { alias: {} }
+
+    // Add path aliases
     if(config.resolve) {
       config.resolve.alias = {
         ...config.resolve.alias,
         '@': path.resolve(__dirname, '../src'),
+        'react-big-calendar': path.resolve(__dirname, '../src'),
       }
     }
+
+    // Add SASS support
+    // if(config.module?.rules) {
+    //   config.module.rules.push({
+    //     test: /\.scss$/,
+    //     use: [
+    //       'style-loader',
+    //       {
+    //         loader: 'css-loader',
+    //         options: {
+    //           modules: {
+    //             auto: true,
+    //             localIdentName: '[name]__[local]--[hash:base64:5]',
+    //           },
+    //         },
+    //       },
+    //       'sass-loader',
+    //     ],
+    //     include: path.resolve(__dirname, '../'),
+    //   })
+    // }
     return config
   },
 }
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,22 +1,34 @@
-export const parameters = {
-  controls: {
-    // TODO: refactor jsDocs in Calendar control
-    //expanded: true,
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+import type { Preview } from '@storybook/react'
+
+const preview: Preview = {
+  parameters: {
+    actions: {
+      // The updated way to match action event handlers
+      includeParameters: /^on[A-Z].*/,
     },
+    controls: {
+      // TODO: refactor jsDocs in Calendar control
+      //expanded: true,
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    // Updated sorting configuration syntax remains similar
+    storySort: {
+      order: [
+        'About Big Calendar',
+        'About Our Examples',
+        'props',
+        'Examples',
+        'Guides',
+        'Addons',
+        ['Introduction', 'props'],
+      ],
+    },
+    // Default view mode remains the same
+    viewMode: 'docs',
   },
-  storySort: {
-    order: [
-      'About Big Calendar',
-      'About Our Examples',
-      'props',
-      'Examples',
-      'Guides',
-      'Addons',
-      ['Introduction', 'props'],
-    ],
-  },
-  viewMode: 'docs',
 }
+
+export default preview
